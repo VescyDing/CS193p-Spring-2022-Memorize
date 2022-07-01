ef6e196802180c4747e51ff2384b0a57aeff0a6f
Dingwenxing <2557544773@qq.com>
2022-07-02 01:12:12 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 丁文星 on 2022/7/2.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
